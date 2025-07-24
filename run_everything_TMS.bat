540451781a4751186299794f78d481abc9c13a8f
NickHananeia <nickhananeia@gmail.com>
2025-07-24 14:14:47 +0200

    Add files via upload

---
+++ run_everything_TMS.bat
@@ -0,0 +1,12 @@
+@echo off
+
+
+cd ".\1HZ\sim1\"
+START neuron TMS_script.hoc
+cd "..\.."
+cd ".\5HZ\sim1\"
+START neuron TMS_script.hoc
+cd "..\.."
+cd ".\10HZ\sim1\"
+START neuron TMS_script.hoc
+cd "..\.."
